3b70178404433171b15faaea17986514d192d79f
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-16 15:31:11 +0530

    Second Commit

--- Products.cs
+++ Products.cs
@@ -15,7 +15,8 @@ namespace MyProduct
             { 2,new ProductProp(){ Name="gulab jamun",Price=150,Brand="rajamandri",ManufacturingDate=new DateTime(day:1,month:5,year:2023),ExporyDate=new DateTime(day:1,month:11,year:2023)} },
             { 3,new ProductProp(){ Name="Coco cola",Price=65,Brand="COla",ManufacturingDate=new DateTime(day:1,month:1,year:2022),ExporyDate=new DateTime(day:1,month:1,year:2025)} },
             { 4,new ProductProp(){ Name="Center fresh",Price=2,Brand="Perfetti",ManufacturingDate=new DateTime(day:1,month:12,year:2023),ExporyDate=new DateTime(day:1,month:12,year:2024)} },
-            { 5,new ProductProp(){ Name="santoor",Price=35,Brand="Wipro",ManufacturingDate=new DateTime(day:1,month:12,year:2023),ExporyDate=new DateTime(day:1,month:12,year:2026)} }
+            { 5,new ProductProp(){ Name="santoor",Price=35,Brand="Wipro",ManufacturingDate=new DateTime(day:1,month:12,year:2023),ExporyDate=new DateTime(day:1,month:12,year:2026)} },
+            { 6,new ProductProp(){ Name="Milk",Price=60,Brand="Arokya",ManufacturingDate=new DateTime(day:16,month:2,year:2024),ExporyDate=new DateTime(day:20,month:2,year:2024)} }
         };
         public SortedList<int,ProductProp> AllProducts()
         {
